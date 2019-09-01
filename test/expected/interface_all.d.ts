dec3b0d9a8fb8e49a3ae78f8bd150f06b3627166
Aidin Abedi <aidinabedi@gmail.com>
2019-09-01 04:02:31 +0200

    support static members/methods in an interface (#95)
    * add tests for static interface members and methods
    
    * implement support for static children in interface/mixin
    
    * add more tests
    
    * cosmetic changes

--- test/expected/interface_all.d.ts
+++ test/expected/interface_all.d.ts
@@ -1,9 +1,48 @@
+declare namespace Color {
+    /**
+     * @function Color.staticMethod1
+     */
+    function staticMethod1(): void;
+    /**
+     * @function
+     * @static
+     */
+    function staticMethod2(): void;
+    /**
+     * @member {Number} Color.staticMember1
+     */
+    var staticMember1: number;
+    /**
+     * @member {Boolean}
+     * @static
+     */
+    var staticMember2: boolean;
+    /**
+    * @type {String}
+    * @name Color.staticMember3
+     */
+    var staticMember3: string;
+    /**
+    * @type {Object}
+    * @static
+     */
+    var staticMember4: any;
+}
+
 /**
  * Interface for classes that represent a color.
  *
  * @interface
  */
 declare interface Color {
+    /**
+     * @function
+     */
+    instanceMethod(): void;
+    /**
+     * @member {Number}
+     */
+    instanceMember: number;
     /**
      * Get the color as an array of red, green, and blue values, represented as
      * decimal numbers between 0 and 1.
--- test/fixtures/interface_all.js
+++ test/fixtures/interface_all.js
@@ -6,6 +6,50 @@
 function Color() {}
 
 /**
+ * @function Color.staticMethod1
+ */
+Color.staticMethod1 = function() {};
+
+/**
+ * @function
+ * @static
+ */
+Color.staticMethod2 = function() {};
+
+/**
+ * @member {Number} Color.staticMember1
+ */
+Color.staticMember1 = 1;
+
+/**
+ * @member {Boolean}
+ * @static
+ */
+Color.staticMember2 = true;
+
+/**
+* @type {String}
+* @name Color.staticMember3
+*/
+Color.staticMember3 = "foobar";
+
+/**
+* @type {Object}
+* @static
+*/
+Color.staticMember4 = {};
+
+/**
+ * @function
+ */
+Color.prototype.instanceMethod = function() {};
+
+/**
+ * @member {Number}
+ */
+Color.prototype.instanceMember = 5;
+
+/**
  * Get the color as an array of red, green, and blue values, represented as
  * decimal numbers between 0 and 1.
  *
