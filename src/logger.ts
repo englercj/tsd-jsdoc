0353bf491ad2788016774e806d91120606605c30
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2020-01-06 14:57:36 +0100

    resolves #113 Added source location information to warnings

--- src/logger.ts
+++ src/logger.ts
@@ -7,12 +7,19 @@ export function setVerbose(value: boolean)
     isVerbose = value;
 }
 
-export function warn(msg: string, data?: any)
+export function warn(msg: string, data?: any|TAnyDoclet)
 {
+
     if (typeof(console) === 'undefined')
         return;
 
-    console.warn(`${header} ${msg}`);
+    let prefix = header;
+    if (arguments.length > 1 && data && data.hasOwnProperty('meta')) {
+        const meta = data.meta;
+        prefix = `${prefix} ${meta.filename}:${meta.lineno}:${meta.columnno}`;
+    }
+
+    console.warn(`${prefix} ${msg}`);
 
     if (isVerbose && arguments.length > 1)
     {
