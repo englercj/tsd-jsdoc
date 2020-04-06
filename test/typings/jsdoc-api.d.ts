e17c69b9c10b839f5dff6bad94614c9e4d4ed9a2
aidinabedi <aidinabedi@gmail.com>
2020-04-06 12:46:19 +0200

    allow tests to log warnings to console by letting spawned jsdoc process inherit stdio

--- test/typings/jsdoc-api.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'jsdoc-api' {
-    export function explain(options: any): Promise<any[]>;
-    export function explainSync(options: any): any[];
-    export function renderSync(options: any): void;
-}
