fe18a8861e93fd2969ac65ee1487d6e430948aa1
Aidin Abedi <aidinabedi@gmail.com>
2020-04-22 20:29:00 +0200

    Add support for ClosureCompiler syntax "typeof T" (#128)
    * add typeof tests
    
    * add support for typeof syntax
    
    * update readme

---
+++ test/expected/typeof_all.d.ts
@@ -0,0 +1,23 @@
+declare class Foobar1 {
+    x: typeof MyClass;
+    y: typeof MyClass;
+    z: string | typeof MyClass;
+    u: typeof MyClass | string;
+    v: (typeof MyClass)[];
+    w: typeof Array;
+    q: typeof Array;
+    r: typeof Class;
+    s: typeof Array;
+}
+
+declare class Foobar2 {
+    x: typeof MyClass;
+    y: typeof MyClass;
+    z: string | typeof MyClass;
+    u: typeof MyClass | string;
+    v: (typeof MyClass)[];
+    w: typeof Array;
+    q: typeof Array;
+    r: typeof Class;
+    s: typeof Array;
+}
---
+++ test/specs/typeof.ts
@@ -0,0 +1,7 @@
+import { expectJsDoc } from '../lib';
+
+suite('Typeof Checks', () => {
+    test('All', () => {
+        expectJsDoc('typeof_all');
+    });
+});
