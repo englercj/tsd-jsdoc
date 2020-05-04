112b3da169c8d5a0af9e2f9df7a65011f71e900f
Bryson Hotopp <bryson.hotopp@life.church>
2020-05-04 09:13:17 -0500

    Add test for quoted module name

---
+++ test/expected/module_quoted.d.ts
@@ -0,0 +1,25 @@
+declare module "1.0/array-to-object-keys" {
+    /**
+     * @param value - The original array entry
+     * @param index - The index of the array entry (starts at 0)
+     */
+    type valueGenerator = (value: string, index: number) => any;
+    /**
+     * Converts an array to an object with static keys and customizable values
+     * @example
+     * arrayToObjectKeys(["a", "b"])
+     * // {a: null, b: null}
+     * @example
+     * arrayToObjectKeys(["a", "b"], "value")
+     * // {a: "value", b: "value"}
+     * @example
+     * arrayToObjectKeys(["a", "b"], (key, index) => `value for ${key} #${index + 1}`)
+     * // {a: "value for a #1", b: "value for b #2"}
+     * @param array - Keys for the generated object
+     * @param [valueGenerator = null] - Optional function that sets the object values based on key and index
+     * @returns A generated object based on the array input
+     */
+    function arrayToObjectKeys(array: string[], valueGenerator?: valueGenerator | any): {
+        [key: string]: any;
+    };
+}
--- test/specs/module.ts
+++ test/specs/module.ts
@@ -3,5 +3,8 @@ import { expectJsDoc } from '../lib';
 suite('Module Checks', () => {
     test('All', () => {
         expectJsDoc('module_all');
-    });
+		});
+		test('Quoted', () => {
+			expectJsDoc('module_quoted');
+	});
 });
